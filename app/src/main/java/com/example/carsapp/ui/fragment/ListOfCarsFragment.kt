15e7ecdfd4a6afc7d2001ce0aeb4d5c4368d1075
GuerraAna <ana.guerrads@hotmail.com>
2023-09-13 16:19:37 -0300

    feature(1.1.0): implement toast to see fab action click result WIP

--- app/src/main/java/com/example/carsapp/ui/fragment/ListOfCarsFragment.kt
+++ app/src/main/java/com/example/carsapp/ui/fragment/ListOfCarsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.carsapp.databinding.CarsFragmentBinding
@@ -67,5 +68,9 @@ internal class ListOfCarsFragment : Fragment() {
 
         binding.carsList.layoutManager = LinearLayoutManager(context)
         binding.carsList.adapter = CarsListAdapter(listOfCars)
+
+        binding.carsFabButton.setOnClickListener {
+            Toast.makeText(context, "Adicionar carro elétrico", Toast.LENGTH_SHORT).show()
+        }
     }
 }
